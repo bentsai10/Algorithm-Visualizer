d0864f25a58000563eaecfd7e247fde231d04900
Ben Tsai <benjamintsa@gmail.com>
2020-09-06 14:20:59 -0400

    initial commit (basic UI set up)

---
+++ sorting/static/javascript/sorting.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    var arr = [];
+    var output = "";
+    for(var i = 0; i <10; i++){
+        arr[i] = Math.floor((Math.random() * 1000) + 1);
+        output += arr[i];
+        output += " ";
+    }
+    $('#array').html(output);
+    $(".sort_select_btn").click(function(){
+        var sort_type = $(this).attr('id');
+        $('#sort_type_value').attr('value', sort_type);
+        var output = "";
+        output += sort_type;
+        output += " Sort";
+        $('#current_sort_type').html(output);
+    })
+    $('#array_len_input').change(function(){
+        var x = $('#array_len').html();
+        arr = [];
+        var output = "";
+        for(var i = 0; i < x; i++){
+            arr[i] = Math.floor((Math.random() * 1000) + 1);
+            output += arr[i];
+            output += " ";
+        }
+        $('#array').html(output);
+    })
+
+    $("#sort_btn").click(function(){
+        console.log($('#sort_type_value').attr('value'));
+        var type = $('#sort_type_value').attr('value');
+        console.log(arr.length);
+        if(type == 'Merge'){
+            console.log('merge sort selected');
+        }
+        else if(type == 'Bubble'){
+            console.log('bubble sort selected');
+        }
+        else if(type == 'Selection'){
+            console.log('selection sort selected');
+        }
+        else if(type == 'Heap'){
+            console.log('heap sort selected');
+        }
+        else if(type == 'Insertion'){
+            console.log('insertion sort selected');
+        }
+        else{
+            console.log('quick sort selected');
+        }
+    })
+ });
+
+
+
